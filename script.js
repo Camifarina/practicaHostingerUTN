64a5c74fd4580267638340e62c60081ddf90db79
camila <camilafarina03@gmail.com>
2025-03-01 15:59:37 -0300

    comentar js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let edad = prompt ('Ingresa tu edad')
+/* let edad = prompt ('Ingresa tu edad')
 let lenguaje = prompt ('Ingresa tu lenguaje favorito')
 
 if(edad > 16 && lenguaje === 'JS'){
     console.log ('Bienvenido al desarrollo web frontend')
 } else{
     console.log ('No se puede ser perfecto en todo')
-}
+} */
+

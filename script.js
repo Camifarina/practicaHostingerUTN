cbc2e728872171dc821bcc8864f2efb04c3a5174
camila <camilafarina03@gmail.com>
2025-03-01 00:13:35 -0300

    ejercicio js

---
+++ script.js
@@ -0,0 +1,8 @@
+let edad = prompt ('Ingresa tu edad')
+let lenguaje = prompt ('Ingresa tu lenguaje favorito')
+
+if(edad > 16 && lenguaje === 'JS'){
+    console.log ('Bienvenido al desarrollo web frontend')
+} else{
+    console.log ('No se puede ser perfecto en todo')
+}
